48f16ba281cc62d1ada4829258306fa0ec7b0519
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-04-26 21:44:29 +0300

    Верстка блока header.

---
+++ javascript/scripts.js
@@ -0,0 +1,12 @@
+// Открытие burger-menu
+var menuBtn = document.querySelector(".burger-menu"),
+  burgerLine = document.querySelectorAll(".burger-menu__line");
+
+burgerLine.forEach(function (line) {
+  menuBtn.addEventListener("click", function () {
+    line.classList.toggle("burger-menu__line-active");
+    document
+      .querySelector(".menu")
+      .classList.toggle("menu-active");
+  });
+});
