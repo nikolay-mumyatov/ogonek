622c6ab5b06ec55ac18e3e75fb76090ab67e59eb
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-05-07 21:22:50 +0300

    Верстка блока с видео. Написание скрипта загрузки видео.

---
+++ javascript/video.js
@@ -0,0 +1,56 @@
+function findVideos() {
+  let videos = document.querySelectorAll(".video");
+
+  for (let i = 0; i < videos.length; i++) {
+    setupVideo(videos[i]);
+  }
+}
+
+function setupVideo(video) {
+  let link = video.querySelector(".video__link");
+  let button = video.querySelector(".video__button");
+
+  console.log(link);
+
+  video.addEventListener("click", () => {
+    let iframe = createIframe();
+    let scriptVideo = createScript();
+
+    link.remove();
+    button.remove();
+    video.appendChild(iframe);
+    video.appendChild(scriptVideo);
+  });
+
+  video.classList.add("video--enabled");
+}
+
+function createIframe() {
+  let srcVideo = document.querySelector(".video__link").getAttribute("href");
+  let iframeVideo = document.createElement("iframe");
+
+
+  iframeVideo.setAttribute("src", srcVideo);
+  iframeVideo.setAttribute(
+    "style",
+    "position:absolute;top:0;left:0;width:100%;height:100%;"
+  );
+  iframeVideo.setAttribute("frameborder", "0");
+  iframeVideo.setAttribute("allow", "autoplay");
+  iframeVideo.setAttribute("allowfullscreen", "");
+  iframeVideo.classList.add("video__media");
+
+  return iframeVideo;
+}
+
+function createScript() {
+
+  let srcVideo = document.querySelector(".video__link").getAttribute("href");
+  let scriptVideo = document.createElement("script");
+
+  scriptVideo.setAttribute("src", srcVideo);
+
+  return scriptVideo;
+}
+
+findVideos();
